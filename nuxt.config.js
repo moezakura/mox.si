10e34b5be7c900e15485d9b92796f84ae8c4ee52
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2020-04-12 00:33:31 +0900

    support og

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,13 +5,23 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    headAttrs: {
+      prefix: 'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/websaite#',
+    },
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
+      /** OG SETTING */
+      {property: 'og:url', 'content': 'https://mox.si'},
+      {property: 'og:type', 'content': 'website'},
+      {property: 'og:title', 'content': process.env.npm_package_name || ''},
+      {property: 'og:description', 'content': process.env.npm_package_description || ''},
+      {property: 'og:site_name', 'content': 'mox.si'},
+      {property: 'og:image', 'content': '/og_logo.png'},
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'icon', type: 'image/png', href: '/favicon.png'},
       {rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto%20Mono'},
     ]
   },
